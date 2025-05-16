1f57030ea948bcffafec1ecbb587e951e70e3549
junchenthompson <junchent@gmail.com>
2025-05-16 11:56:15 -0700

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,37 +1,14 @@
-export default function handler(req, res) {
-  const header = {
-    alg: "HS256",
-    typ: "JWT",
-    kid: "68261dae6ab8fa0001a116bb"
-  };
-
+module.exports = (req, res) => {
   const payload = {
     iat: Math.floor(Date.now() / 1000),
     exp: Math.floor(Date.now() / 1000) + 5 * 60,
-    aud: "/admin/"
   };
 
-  const base64url = (str) =>
-    Buffer.from(JSON.stringify(str))
-      .toString("base64")
-      .replace(/=/g, "")
-      .replace(/\+/g, "-")
-      .replace(/\//g, "_");
-
-  const unsignedToken = base64url(header) + "." + base64url(payload);
-
-  const crypto = require("crypto");
-  const secret = Buffer.from("305710084c895aa9f34bbd49361a7f8c1829033f95acd4e1dcecab766033efa3", "hex");
-
-  const signature = crypto
-    .createHmac("sha256", secret)
-    .update(unsignedToken)
-    .digest("base64")
-    .replace(/=/g, "")
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_");
-
-  const token = unsignedToken + "." + signature;
+  const token = [
+    'header',
+    Buffer.from(JSON.stringify(payload)).toString('base64url'),
+    'signature',
+  ].join('.');
 
   res.status(200).json({ token });
-}
+};
